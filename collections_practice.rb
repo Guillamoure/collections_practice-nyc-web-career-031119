ae2205933939974f5a68cf66aea196a79b0fe647
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-12 12:59:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,9 +9,9 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  new_array = []
+  
   array.sort do |value|
-    new_array =value.length
+    value.length
   end
   new_array.reverse
 end
